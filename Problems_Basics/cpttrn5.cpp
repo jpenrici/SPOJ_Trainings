bfbc214a132cae3cefd4058a6ff45c8aa4a1f8ce
Juan Pablo Enrici <jpenrici@gmail.com>
2018-08-03 19:09:22 -0300

    Add files via upload

---
+++ Problems_Basics/cpttrn5.cpp
@@ -0,0 +1,89 @@
+// https://www.spoj.com/problems/CPTTRN5/
+// CPTTRN4 - Character Patterns (Act 5)
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t(0), l(0), c(0), s(0);
+    std::vector<std::string> v_out {};
+
+    std::cin >> t;
+    while(t > 0) {
+        std::cin >> l >> c >> s;
+        string asterisk_line = string((s+1)*c+1, char(42));
+        string base = string(s+1, char(46));
+        base[0] = char(42);
+
+        for (int i = 0; i < l; ++i) {
+            v_out.push_back(asterisk_line);
+            for (int j = 0; j < s; ++j) {
+                string line("");
+                for (int k = 0; k < c; ++k) {
+                    string str(base);
+                    if (i % 2 == 0) {
+                        if (k % 2 == 0) str[j + 1] = char(92);
+                        else str[s - j] = char(47);
+                    } 
+                    else {
+                        if (k % 2 != 0) str[j + 1] = char(92);
+                        else str[s - j] = char(47);         
+                    }
+                    line.append(str);
+                }
+                line.push_back(char(42));
+                v_out.push_back(line);
+            }
+        }
+        v_out.push_back(asterisk_line);
+        v_out.push_back("");
+        std::cin.clear();
+        t--;
+    }
+
+    for (std::string output: v_out)
+        std::cout << output << '\n';
+
+    return 0;
+}
+
+
+// Example
+
+// Input:
+// 3
+// 3 1 2 
+// 4 4 1 
+// 2 5 2 
+
+// Output:
+// ****
+// *\.*
+// *.\*
+// ****
+// *./*
+// .*
+// ****
+// *\.*
+// *.\*
+// ****
+
+// *********
+// *\*/*\*/*
+// *********
+// */*\*/*\*
+// *********
+// *\*/*\*/*
+// *********
+// */*\*/*\*
+// *********
+
+// ****************
+// *\.*./*\.*./*\.*
+// *.\*/.*.\*/.*.\*
+// ****************
+// *./*\.*./*\.*./*
+// */.*.\*/.*.\*/.*
+// ****************
