280fb2e2ebaeda101e9b091f4b8994c213002e80
jpenrici <jpenrici@gmail.com>
2018-08-10 18:25:14 -0300

    Update
    Update

--- Problems_Basics/cpttrn7.cpp
+++ Problems_Basics/cpttrn7.cpp
@@ -1,6 +1,7 @@
 // https://www.spoj.com/problems/CPTTRN7/
 // CPTTRN7 - Character Patterns (Act 7)
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,31 +12,27 @@ int main()
     cin >> t;
     while(t--) {
         cin >> l >> c >> s;
+        
+        vector<string> v_out(2 * s);
+        for (int i = 0; i < s; ++i) {
+            string str_up(""), str_down("");
+            string str_border = string(s - i - 1, char(46));
+            string str_center = string(2 * i, char(46));
+            for (int j = 0; j < c; ++j) {
+                str_up += str_border + char(47) + str_center
+                    + char(92) + str_border; 
+                str_down += str_border + char(92) + str_center
+                    + char(47) + str_border;                          
+            }
+            v_out[i] = str_up;
+            v_out[2 * s - i - 1] = str_down;
+        }
 
         while(l--) {
-            int center = 0;
-            for (int i = 0; i < s; ++i) {
-                string str_border = string(s - i - 1, char(46));
-                string str_center = string(center, char(46));
-                center = center + 2;
-                for (int j = 0; j < c; ++j) {
-                    cout << str_border << char(47) << str_center
-                         << char(92) << str_border;
-                }
-                cout << endl;
-            }
-            center = center - 2;
-            for (int i = 0; i < s; ++i) {
-                string str_border = string(i, char(46));
-                string str_center = string(center, char(46));
-                center = center - 2;
-                for (int j = 0; j < c; ++j) {
-                    cout << str_border << char(92) << str_center
-                         << char(47) << str_border;
-                }
-                cout << endl;
-            }
+            for (string s: v_out) 
+                cout << s << "\n";
         }
+
         cin.clear();
     }
 
